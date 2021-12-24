c43a731057ab6c5a6c6618992391c3f225fad05e
tonick2001 <tonick2001@inbox.ru>
2021-12-24 11:39:03 +0500

    Create Change_windows_service_service_account.ps1

---
+++ Change_windows_service_service_account.ps1
@@ -0,0 +1,23 @@
+﻿[string]$username = "User"
+[string]$pwd = "passWord"
+[string]$service="AdobeARMservice"
+
+function Change-ServiceAccount
+{
+    param (
+        [string]$account,
+        [string]$password,
+        [string]$service
+
+    )
+    
+    $temp = "name='$service'"
+    $svc=gwmi win32_service -filter $temp
+    $svc.StopService()
+    Start-Sleep -s 5
+    $svc.change($null,$null,$null,$null,$null,$null,$account,$password,$null,$null,$null)
+    $svc.StartService()
+}
+
+
+Change-ServiceAccount -account $username -password $pwd -service $service
