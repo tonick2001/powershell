bb7cc59ce131ba9823e955d0916915c74ead66f9
tonick2001 <tonick2001@inbox.ru>
2021-12-30 16:28:17 +0500

    Create Set-IISanonymousAuthenticationIdentity.ps1

---
+++ Set-IISanonymousAuthenticationIdentity.ps1
@@ -0,0 +1,21 @@
+﻿function Set-IISanonymousAuthenticationIdentity
+{
+    param (
+        [string]$user, 
+        [string]$pwd , 
+        [string]$iisSiteName
+       )
+
+
+    Import-Module WebAdministration
+        
+    Set-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication  -Name Enabled -value True -PSPath "IIS:\Sites\$iisSiteName\"
+    Set-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication  -Name username -value $user -PSPath "IIS:\Sites\$iisSiteName\"
+    Set-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication  -Name password -value $pwd -PSPath "IIS:\Sites\$iisSiteName\"
+
+}
+
+Set-IISanonymousAuthenticationIdentity -user "./Администратор" -pwd "P@ssw0rd" -iisSiteName "Default Web Site\RDWeb"
+
+#get-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication   -PSPath "IIS:\Sites\$iisSiteName" -Name Enabled
+#get-webconfigurationproperty /system.webServer/security/authentication/anonymousAuthentication   -PSPath "IIS:\Sites\$iisSiteName" -Name username
