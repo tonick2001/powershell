4e2f86f41b854a68534e840959728e880f9672a3
tonick2001 <tonick2001@inbox.ru>
2021-12-30 13:38:30 +0500

    Create Set-IISPoolIdentity.ps1

---
+++ Set-IISPoolIdentity.ps1
@@ -0,0 +1,17 @@
+﻿function Set-IISPoolIdentity
+{
+    param (
+        [string] $poolname,
+        [string] $user,
+        [string] $pwd
+    )
+    import-module webadministration
+    
+    $pool = Get-Item "IIS:\AppPools\$poolname"
+    $pool.processmodel.identityType = 3
+    $pool.processmodel.username  = $user
+    $pool.processmodel.password = 
+    $pool | set-item
+}
+
+Set-IISPoolIdentity -user ".\Администратор" -pwd "P@ssw0rd" -poolname "RDWebAccess" 
