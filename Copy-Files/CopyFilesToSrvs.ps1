a283dc99324995dd574890a1c1522a2af0c83a6d
tonick2001 <tonick2001@inbox.ru>
2022-01-13 15:14:48 +0500

    new script

---
+++ Copy-Files/CopyFilesToSrvs.ps1
@@ -0,0 +1,23 @@
+﻿#import external variables
+$mypath = $MyInvocation.MyCommand.Path
+$mypath = Split-Path $mypath -Parent
+
+$servers = @("l01-crm-trns01", "l01-TELRC01")
+
+$foldername = "RestoreScripts\"
+$src_file = $mypath+"\files\test.txt"
+
+foreach ($srv in $servers)
+{
+    $pathsrv = "\\"+$srv+"\C$\"+$foldername
+    
+    if (!(Test-Path -Path $pathsrv))
+    {
+       New-Item -Path $pathsrv -ItemType Directory
+       Copy-Item -Path $src_file -Destination $pathsrv
+    }
+    else
+    {
+       Copy-Item -Path $src_file -Destination $pathsrv 
+    }
+}
