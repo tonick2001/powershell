3cdac0a14d7535ebe66860bd9b0a45eb01ec6f18
tonick2001 <tonick2001@inbox.ru>
2022-01-25 23:45:56 +0500

    Create Add_RegFiles.ps1

---
+++ Set-RegEditValues/Add_RegFiles.ps1
@@ -0,0 +1,24 @@
+﻿<#
+    This is script add HKU Regedit values if users logoff
+#>
+
+$userlogin ="admin" 
+
+
+try {
+        New-PSDrive HKU Registry HKEY_USERS -ErrorAction Stop
+    }
+catch {
+    Write-Host "Disk map error"
+
+    }
+finally {
+    #uses WMI get SID
+    $sid=(Get-CimInstance -Class win32_userAccount -Filter "name='$userlogin'").SID    
+    #$sid=(Get-LocalUser -Name "admin" | Select-Object Sid).SID
+    $pathreg = "\"+$sid+"\Software\Business Objects\Suite 11.5\Crystal Reports\Export\Pdf"
+
+    New-Item -Path HKU:$pathreg -Force | Out-Null
+    New-ItemProperty -Path HKU:$pathreg -Name "ForceLargerFonts" -PropertyType "dword" -Value "00000001" 
+    }
+
