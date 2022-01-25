e660679474b80f27e1ef4717a8d3de75cae6c132
tonick2001 <tonick2001@inbox.ru>
2022-01-25 20:28:45 +0500

    Create Get-SIDUser.ps1

---
+++ Get-SIDUser/Get-SIDUser.ps1
@@ -0,0 +1,9 @@
+﻿function get-sid
+{
+    Param ( $DSIdentity )
+    $ID = new-object System.Security.Principal.NTAccount($DSIdentity)
+    return $ID.Translate( [System.Security.Principal.SecurityIdentifier] ).toString()
+}
+ $admin = get-sid "test01"
+ #$admin.SubString(0, $admin.Length - 4)
+ $admin.SubString(0, $admin.Length)
