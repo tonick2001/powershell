e78b251d426728e12d0019b926255a1806f25f34
tonick2001 <tonick2001@inbox.ru>
2022-02-07 12:43:56 +0500

    Create PSSession_invoke.ps1

---
+++ Start_Remote_Script/PSSession_invoke.ps1
@@ -0,0 +1,13 @@
+﻿$login="testuser"
+$pass = ConvertTo-SecureString "Parol22" -AsPlainText -Force  
+$doman = "domain.net"
+
+$duser=$domain+"\"+$login
+
+
+
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $duser, $pass
+
+$s = New-PSSession -ComputerName "L01-MG02" -Credential $cred
+Invoke-Command -Session $s -ScriptBlock {C:\RestoreScripts\05_MsmqQueue.ps1}
+Remove-PSSession -Session $s
