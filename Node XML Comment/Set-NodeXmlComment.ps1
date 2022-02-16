5a7f542b33ad4a5a8a10a7af44b6b70fb460b511
tonick2001 <tonick2001@inbox.ru>
2022-02-16 11:34:51 +0500

    Create Set-NodeXmlComment.ps1

---
+++ Node XML Comment/Set-NodeXmlComment.ps1
@@ -0,0 +1,13 @@
+﻿ #Get xml content
+    $WFMInetpubNVGWeb_content=[xml] (Get-Content -Encoding UTF8 -Path $wfm_inetpubnvgweb_src)
+
+    #Get prod values and change test values
+    try
+    {   
+        $nodeproxy = $WFMInetpubNVGWeb_content.SelectSingleNode('//proxy')
+        $nodeproxy.ParentNode.InnerXml = $nodeproxy.ParentNode.InnerXml.Replace($nodeproxy.OuterXml, $nodeproxy.OuterXml.Insert(0,"<!--").Insert($nodeproxy.OuterXml.Length+4, "-->"))
+    }
+    catch
+    {
+        Add-WriteLog -logpath "$mypath\Log" -logname $namelog -text "The [proxy] property was not found!"
+    }
