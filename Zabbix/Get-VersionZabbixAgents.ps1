cb686172b822e4990e18cbe1919399e5b618cb21
tonick2001 <tonick2001@inbox.ru>
2022-02-19 23:32:53 +0500

    Create Get-VersionZabbixAgents.ps1

---
+++ Zabbix/Get-VersionZabbixAgents.ps1
@@ -0,0 +1,27 @@
+﻿
+#Скрипт просмотра агентов на серверах!
+cls
+$Error.clear()
+
+$servers = @("L01-CRM01","L01-CRM02","L01-CRM03","L01-TEL01")
+
+$hashtable = @{}
+foreach ($srv in $servers)
+{
+    $srt= Invoke-Command -ComputerName $srv -ScriptBlock {$cmd =  "C:\zabbix_agent\bin\win64\zabbix_agentd.exe" 
+                 $cmdex=& $cmd -V
+                 [string]$versionstring=$cmdex | Select-String "(\d\.\d\.\d)" 
+                 return $versionstring}
+    try
+    {
+        
+         $hashtable.Add($srv,($srt.Substring(38,6).Trim()))
+    }
+    catch {Write-Host $Error}
+}
+
+Write-Host "Версия Zabbix агента на серверах:"
+$hashtable | ft
+
+
+
