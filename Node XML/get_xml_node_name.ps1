88e907dcaf4f5519e82ed3f5299aca8f1e000a6a
tonick2001 <tonick2001@inbox.ru>
2023-02-11 19:45:44 +0500

    new scripts

---
+++ Node XML/get_xml_node_name.ps1
@@ -0,0 +1,23 @@
+﻿$xml = [xml]'<Board Title="Система управления взаимоотношениями с клиентами" panels_height="">
+  <Panel allow_roles="" allow_users="" deny_roles="" deny_users="" SecurityFunction="Customer" Name="Customer" Title="Абоненты" Src="http://test.net/Customer/Search" />
+</Board>'
+
+$panels = Select-Xml -Xml $xml -XPath "//Panel"
+$ar = @();
+foreach ($panel in $panels.Node) {
+ 
+  $test = $panel.OuterXml -split " ";
+ 
+  foreach ($param in $test)
+  {
+    if ($param -match '\w+=')
+    {
+        $matches = [regex]::Matches($param, "\w+=")
+        $matches = [regex]::Matches($matches.value, "\w+(?=)")
+        $ar += $matches.value
+    }
+
+  }
+
+  $ar
+}
